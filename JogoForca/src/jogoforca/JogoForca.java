a847d868fc419de53aca648602b9a5a48c1bff73
Marcos <Marcos@marcos-note>
2016-12-03 19:50:33 -0200

    19:50

--- JogoForca/src/jogoforca/JogoForca.java
+++ JogoForca/src/jogoforca/JogoForca.java
@@ -17,5 +17,6 @@ public class JogoForca {
     public static void main(String[] args) {
         System.out.println("version2");
         System.out.println("version2");
+         System.out.println("version2");
     }
 }
