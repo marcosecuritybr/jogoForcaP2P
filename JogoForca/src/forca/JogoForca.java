50b3923b4134e52f14b5108d3dc74caf6a5a5a58
Marcos <Marcos@marcos-note>
2016-12-03 20:15:48 -0200

    20:15

---
+++ JogoForca/src/forca/JogoForca.java
@@ -0,0 +1,16 @@
+
+package forca;
+
+import Front.Menu;
+
+/**
+ *
+ * @author Marcos
+ */
+public class JogoForca {
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+    
+}
